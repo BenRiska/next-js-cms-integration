ffe1a7118333833e63fca8e7e00db574f63f8af6
bentaylor <ben10taylor10@hotmail.com>
2021-03-01 15:49:09 +0000

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => (
+    <footer>
+        <div>© 2020 - Alex The Entreprenerd - Development Sponsored by 
+        <a 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            href="https://magic.link/"
+        > Magic </a>
+        </div>
+    </footer>
+)
+
+export default Footer
